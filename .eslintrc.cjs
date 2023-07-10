dfe1ca34eba7addcdc82a0b1656130cd9077db93
Szymczok <filipszymala@gmail.com>
2023-07-10 12:46:02 +0200

    Fixed auto sorting imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,16 +13,4 @@ module.exports = {
     'react-refresh/only-export-components': 'warn',
   },
   plugin: ["simple-import-sort"],
-  rules: {
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/imports": ["error", {
-      groups: [
-        ["^react"],
-        ["^antd"],
-        ["^@?\\w"],
-        ["@/(.*)"],
-        ["^[./]"]
-      ]
-    }] 
-  }
 }
