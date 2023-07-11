1bd4382449e8ddc4df93e44d147b0145445354c7
Ania <nnakocaj@gmail.com>
2023-07-11 15:30:17 +0200

    Adding AddNewDevice component with a working Dialog (pop-up).

--- src/components/AddNewDevice.jsx
+++ src/components/AddNewDevice.jsx
@@ -1,27 +1,77 @@
 import React from "react";
-import { IconButton, Typography } from "@mui/material";
-import AddBox from '@mui/icons-material/AddBox';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
+
+import AddBox from "@mui/icons-material/AddBox";
+import { Button, IconButton, Typography } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
 
 function AddNewDevice() {
 
-    return (
-    <Card sx={{ minWidth: 275 }}>
-    <CardContent>
-    <Typography variant="h1" sx={{ fontSize: 14 }}>
-        Add new device
-    </Typography>
-    </CardContent>
-    <CardActions>
-    <IconButton aria-label="add" disabled color="primary" size="large">
-    <AddBox />
-    </IconButton>
-    </CardActions>
+    const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  return (
+    <div>
+      <Card sx={{ minWidth: 275, backgroundColor: "#E9E9EF", height: "10rem" }}>
+      <CardContent>
+        <Typography variant="h1" sx={{ fontSize: 14 }}>
+          Add new device
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton>
+            <AddBox onClick={handleClickOpen} />
+        </IconButton>
+      </CardActions>
     </Card>
-    );
-  }
-  
-  export default AddNewDevice;
-  
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Add new device</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            This works !!!!
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="device_id"
+            label="Device ID"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="device_name"
+            label="Device Name"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Submit</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default AddNewDevice;
+
