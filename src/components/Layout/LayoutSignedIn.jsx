d998f0aa9d888c7a2bccf9676d58cae30be5066d
Ania <nnakocaj@gmail.com>
2023-07-14 09:07:34 +0200

    Added Header to LayoutSignedIn

--- src/components/Layout/LayoutSignedIn.jsx
+++ src/components/Layout/LayoutSignedIn.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 
+import Header from "../../components/Layout/components/Header";
 import MiniDrawer from "./components/SideDrawer";
 import Footer from "src/components/Layout/components/Footer";
 
 function Layout() {
   return (
     <>
+      <Header />
       <MiniDrawer />
       <main>
         <Outlet />
